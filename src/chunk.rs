25547dbc8311f6b568dd9125c9b542e4159c8aee
jgage930 <jackgage@pop-os.localdomain>
2023-01-30 16:06:31 -0600

    chunk::new() is implemented and passing tests

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,43 +1,52 @@
-
 use std::convert::TryFrom;
 use std::fmt;
 use std::io::{BufReader, Read};
+use std::str;
+
+use crate::chunk_type::ChunkType;
 
 use crate::{Error, Result};
-use crate::png::ChunkType;
+
+const MAXIMUM_SIZE: u32 = (1 << 31) - 1;
 
 /// A validated PNG chunk. See the PNG Spec for more details
 /// http://www.libpng.org/pub/png/spec/1.2/PNG-Structure.html
 #[derive(Debug, Clone)]
 pub struct Chunk {
-    // Write me!
+    length: u32,
+    chunk_type: ChunkType,
+    chunk_data: Vec<u8>,
+    crc: u32,
 }
 
 impl Chunk {
     /// The length of the data portion of this chunk.
     pub fn length(&self) -> u32 {
-        todo!()
+        self.length
     }
 
     /// The `ChunkType` of this chunk
     pub fn chunk_type(&self) -> &ChunkType {
-        todo!()
+        &self.chunk_type
     }
 
     /// The raw data contained in this chunk in bytes
     pub fn data(&self) -> &[u8] {
-        todo!()
+        &self.chunk_data
     }
 
     /// The CRC of this chunk
     pub fn crc(&self) -> u32 {
-        todo!()
+        self.crc
     }
 
     /// Returns the data stored in this chunk as a `String`. This function will return an error
     /// if the stored data is not valid UTF-8.
     pub fn data_as_string(&self) -> Result<String> {
-        todo!()
+        match str::from_utf8(&self.chunk_data) {
+            Ok(v) => Ok(v.to_owned()),
+            Err(e) => Err(Box::new(fmt::Error)),
+        }
     }
 
     /// Returns this chunk as a byte sequences described by the PNG spec.
@@ -47,7 +56,66 @@ impl Chunk {
     /// 3. The data itself *(`length` bytes)*
     /// 4. The CRC of the chunk type and data *(4 bytes)*
     pub fn as_bytes(&self) -> Vec<u8> {
-        todo!()
+        todo!();
+    }
+
+    pub fn new(chunk_type: ChunkType, chunk_data: Vec<u8>) -> Self {
+        let bytes_for_crc = [&chunk_type.bytes(), &chunk_data[..]].concat();
+        Self {
+            length: chunk_data.len() as u32,
+            chunk_type,
+            chunk_data,
+            crc: crc::crc32::checksum_ieee(&bytes_for_crc),
+        }
+    }
+}
+
+impl TryFrom<&[u8]> for Chunk {
+    type Error = Error;
+
+    fn try_from(bytes: &[u8]) -> Result<Self> {
+        // set up a buffer reader
+        let mut reader = BufReader::new(bytes);
+        let mut buf: [u8; 4] = [0; 4];
+
+        // read the first 4 bytes as length
+        reader.read_exact(&mut buf)?;
+        let length = u32::from_be_bytes(buf);
+
+        if length > MAXIMUM_SIZE {
+            return Err(Box::new(fmt::Error));
+        }
+
+        // read the chunk type
+        reader.read_exact(&mut buf)?;
+        let chunk_type = ChunkType::try_from(buf)?;
+
+        // read the data from the given length
+        let mut chunk_data = vec![0; usize::try_from(length)?];
+        reader.read_exact(&mut chunk_data)?;
+
+        // read the crc bytes
+        reader.read_exact(&mut buf)?;
+        let crc = u32::from_be_bytes(buf);
+
+        Ok(Self {
+            length,
+            chunk_type,
+            chunk_data,
+            crc,
+        })
+    }
+}
+
+impl fmt::Display for Chunk {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "Chunk {{",)?;
+        writeln!(f, "  Length: {}", self.length())?;
+        writeln!(f, "  Type: {}", self.chunk_type())?;
+        writeln!(f, "  Data: {} bytes", self.data().len())?;
+        writeln!(f, "  Crc: {}", self.crc())?;
+        writeln!(f, "}}",)?;
+        Ok(())
     }
 }
 
@@ -71,14 +139,16 @@ mod tests {
             .chain(crc.to_be_bytes().iter())
             .copied()
             .collect();
-        
+
         Chunk::try_from(chunk_data.as_ref()).unwrap()
     }
 
     #[test]
     fn test_new_chunk() {
         let chunk_type = ChunkType::from_str("RuSt").unwrap();
-        let data = "This is where your secret message will be!".as_bytes().to_vec();
+        let data = "This is where your secret message will be!"
+            .as_bytes()
+            .to_vec();
         let chunk = Chunk::new(chunk_type, data);
         assert_eq!(chunk.length(), 42);
         assert_eq!(chunk.crc(), 2882656334);
@@ -173,29 +243,9 @@ mod tests {
             .chain(crc.to_be_bytes().iter())
             .copied()
             .collect();
-        
+
         let chunk: Chunk = TryFrom::try_from(chunk_data.as_ref()).unwrap();
-        
+
         let _chunk_string = format!("{}", chunk);
-    }
-}
-
-impl TryFrom<&[u8]> for Chunk {
-    type Error = Error;
-
-    fn try_from(bytes: &[u8]) -> Result<Self> {
-        todo!()
-    }
-}
-
-impl fmt::Display for Chunk {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "Chunk {{",)?;
-        writeln!(f, "  Length: {}", self.length())?;
-        writeln!(f, "  Type: {}", self.chunk_type())?;
-        writeln!(f, "  Data: {} bytes", self.data().len())?;
-        writeln!(f, "  Crc: {}", self.crc())?;
-        writeln!(f, "}}",)?;
-        Ok(())
     }
 }
