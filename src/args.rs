cd48ff3735387e4cf247cebc6097f644d82e32ea
jgage930 <jackgage@pop-os.localdomain>
2023-02-08 20:27:30 -0600

    commit before impl trait for value parser factory

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,33 @@
+use clap::Parser;
 use std::path::PathBuf;
 
+use crate::chunk_type::ChunkType;
+
 pub enum PngMeArgs {
-    Encode,
-    Decode,
-    Remove,
-    Print,
+    Encode(EncodeArgs),
+    Decode(DecodeArgs),
+    Remove(RemoveArgs),
+    Print(PrintArgs),
 }
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct EncodeArgs {
+    #[arg(short, long, help = "Path to input file")]
+    file_path: PathBuf,
+
+    #[arg(short, long)]
+    chunk_type: ChunkType,
+
+    #[arg(short, long)]
+    message: String,
+
+    #[arg(short, long)]
+    output_file: PathBuf,
+}
+
+pub struct DecodeArgs {}
+
+pub struct RemoveArgs {}
+
+pub struct PrintArgs {}
